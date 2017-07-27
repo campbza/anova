e094ce0d0fb08273f571cabb9fb2311a56432da5
Zachary Sean Campbell <campbza@ravioli.reed.edu>
2017-07-27 16:05:15 -0700

    ptr changes

--- ptr.py
+++ ptr.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-def noisy_size_estimate(data, lb, epsilon, delta):
-    xs = [len(data[i]) for i in range(len(data))]
-    noisy_xs = [abs(i + np.random.laplace(0.0,1.0/epsilon)) for i in xs]
-    noisy_min = min(noisy_xs)
-    d = abs(lb - noisy_min)
-    cdf = 1 - 1/2 * exp(- d/(1.0/epsilon))
-    
+def cdf(x, m, b):
+    if x < m:
+        return (1.0/2.0)*np.exp((x-m)/b)
+    else:
+        return 1.0 - (1.0/2.0)*np.exp((-x-m)/b)
+
+def noisy_size_estimate(data, epsilon, delta):
+    xs = list(data.values())
+    m = min([len(xs[i]) for i in range(len(xs))])
+    noisy_m = m + np.random.laplace(0.0,1.0/epsilon)
+    gap = -np.log(2*delta)
+    working_m = noisy_m - gap
+    return working_m 
