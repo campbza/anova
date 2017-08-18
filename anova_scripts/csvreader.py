adbb164e64fd466ac2c91669367315b3ea6fd447
Zachary Campbell <campbza@reed.edu>
2017-08-17 21:49:15 -0700

    comments to csvreader.py

--- anova_scripts/csvreader.py
+++ anova_scripts/csvreader.py
@@ -26,7 +26,9 @@ def plot_lines(sizes, eps_vals, significance, outfile, threshold, title):
     print('Wrote to ' + outfile)
     return
 
-def pvals_significance(infile, threshold, outfile, title):
+def pvals_significance(infile, outfile, graphtitle, threshold):
+#input: csv file, outfile name (.png), graphtitle, and threshold for significant pvals
+#output: graph of infile data
     file_contents = open(infile, 'r')
     reader = csv.reader(file_contents)
     pval_dict = {}
@@ -35,16 +37,16 @@ def pvals_significance(infile, threshold, outfile, title):
     for line in reader:
         size = int(line[0])
         if size not in sizes:
-            sizes.append(size)
+            sizes.append(size) #gather list of sizes (these are total size of the database)
         epsilon = line[3]
         if epsilon not in epsilons:
-            epsilons.append(epsilon)
+            epsilons.append(epsilon) #gather list of epsilon values
         key = (size,epsilon)
-        if key in pval_dict:
-            pval_dict[key].append(float(line[4]))
+        if key in pval_dict: #populate dictionary keyed by size,epsilon tuples
+            pval_dict[key].append(float(line[4])) #line[4] in csv is the pval
         else:
             pval_dict[key] = [float(line[4])]
-    for item in pval_dict:
+    for item in pval_dict: #replace the lists with a percent of how many in that list are significant
         l = len(pval_dict[item])
         sig_pvals = [x for x in pval_dict[item] if x < threshold]
         m = len(sig_pvals)
@@ -52,7 +54,7 @@ def pvals_significance(infile, threshold, outfile, title):
     sizes.sort()
     epsilon_dict = {}
     labels = []
-    significance = []
+    significance = [] #list of list of percent significance, each list corresponding to epsilon
     for e in epsilons:
         epsilon_dict[e] = []
     for s in sizes:
@@ -60,5 +62,5 @@ def pvals_significance(infile, threshold, outfile, title):
             epsilon_dict[e].append(pval_dict[(s,e)])
             if epsilon_dict[e] not in significance:
                 significance.append(epsilon_dict[e])
-    plot_lines(sizes, epsilons, significance, outfile, threshold, title)
+    plot_lines(sizes, epsilons, significance, outfile, threshold, graphtitle)
     return 
